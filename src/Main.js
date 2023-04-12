cd937aa16fa3077033444815af16e642a7ceade2
jchong <rockportereport@gmail.com>
2023-04-12 09:38:10 +0800

    project init & environment setup

---
+++ src/Main.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Main = () => {
+  return (
+    <div className="default">main 123</div>
+  )
+}
+
+export default Main
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path=require("path");
+
+module.exports={
+    // 建置的模式
+    mode:"development", // 預設 production
+    // 入口
+    entry:"./src/app.js",
+    // 輸出
+    output:{
+        filename:"bundle.js",
+        path:path.resolve(__dirname, "dist")
+    },
+    // DevServer 設定
+    devServer:{
+        static:"./dist",
+        hot: true,
+        historyApiFallback: true,
+    },
+    // 模組載入規則
+    module:{
+        rules:[
+            // CSS 樣式表載入規則
+            {
+                test:/\.css$/i,
+                use:["style-loader", "css-loader"]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            }
+        ]
+    }
+};
